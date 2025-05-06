ee7980a0d77cf32c4373b3bfcacd51dae7848664
yes4save <yes4save@gmail.com>
2025-05-06 22:10:03 +0530

    Update welcome.tsx

--- app/components/welcome.tsx
+++ app/components/welcome.tsx
@@ -2,7 +2,6 @@ import { BookHeart, Goal, ArrowRight } from 'lucide-react';
 import Link from 'next/link';
 import Image from 'next/image';
 
-
 interface Feature {
   id: string;
   title: string;
@@ -20,7 +19,6 @@ interface Feature73Props {
 }
 
 const Welcome = ({
-  
   features = [
     {
       id: "feature-1",
@@ -33,43 +31,43 @@ const Welcome = ({
     {
       id: "feature-2",
       title: "OUR MISSION",
-      description:"Our mission is to ignite the fire of God’s Spirit  through evangelism, revival events and humanitarian outreach. We are dedicated to transforming lives,  spreading the Gospel and bringing  hope to those in need.",
+      description:
+        "Our mission is to ignite the fire of God’s Spirit through evangelism, revival events and humanitarian outreach. We are dedicated to transforming lives, spreading the Gospel and bringing hope to those in need.",
       image: "1.jpg",
-      icon: <Goal className="size-7 shrink-0 ml-2" />
+      icon: <Goal className="size-7 shrink-0 ml-2" />,
     },
   ],
 }: Feature73Props) => {
   return (
-    <section className='pb-20'>
+    <section className="pb-20">
       <div className="container flex flex-col lg:gap-16 gap-10 lg:px-36 mx-auto">
-
-      <div className="text-center mt-14 bg-gray-200 py-10">
-      <h2 className="text-3xl font-semibold text-pretty md:mb-4 md:text-4xl lg:mb-2 lg:max-w-3xl lg:text-3xl mx-auto text-blue-900 underline-offset-8 underline pb-10 -mt-10 pt-8">
-
-                  WHO WE ARE?
-                </h2>
-          <p className="lg:text-2xl lg:px-20 font-(family-name:--font-unna) text-gray-800 px-5">Welcome to  <span className='text-red-700'>Elijah&apos;s Fire Faith Ministry and Yes4save Foundation</span>, where our mission is to ignite the fire of God&apos;s Spirit through evangelism, revival events and humanitarian outreach. We are dedicated to transforming lives, spreading the Gospel and bringing hope to those in need.</p>
+        <div className="text-center mt-14 bg-gray-200 py-10">
+          <h2 className="text-3xl font-semibold text-pretty md:mb-4 md:text-4xl lg:mb-2 lg:max-w-3xl lg:text-3xl mx-auto text-blue-900 underline-offset-8 underline pb-10 -mt-10 pt-8">
+            WHO WE ARE?
+          </h2>
+          <p className="lg:text-2xl lg:px-20 font-(family-name:--font-unna) text-gray-800 px-5">
+            Welcome to <span className="text-red-700">Elijah&apos;s Fire Faith Ministry and Yes4save Foundation</span>, where our mission is to ignite the fire of God&apos;s Spirit through evangelism, revival events and humanitarian outreach. We are dedicated to transforming lives, spreading the Gospel and bringing hope to those in need.
+          </p>
         </div>
-        
+
         <div className="grid gap-6 md:grid-cols-2 lg:gap-8 -mt-4">
-          
-          {features.slice(0).map((feature) => (
+          {features.map((feature) => (
             <div
               key={feature.id}
               className="flex flex-col overflow-clip rounded-sm border border-border bg-gray-200"
             >
-              <div>
+              <div className="relative aspect-[16/9] w-full">
                 <Image
-                  src={feature.image}
+                  src={`/${feature.image}`}
                   alt={feature.title}
-                  className="aspect-[16/9] h-full w-full object-cover object-center"
-                  width={0}
-  height={0}
+                  fill
+                  className="object-cover object-center"
                 />
               </div>
               <div className="px-6 py-8 md:px-8 md:py-10 lg:px-10 lg:py-12">
                 <h3 className="mb-3 text-lg font-medium md:mb-4 md:text-2xl lg:mb-6 flex items-center text-blue-900 underline-offset-8 underline">
-                  {feature.title}{feature.icon}
+                  {feature.title}
+                  {feature.icon}
                 </h3>
                 <p className="text-gray-800 lg:text-xl font-(family-name:--font-unna)">
                   {feature.description}
@@ -78,19 +76,16 @@ const Welcome = ({
             </div>
           ))}
         </div>
-      </div>
-      <Link href='/about/y4s' legacyBehavior>
-      <a
-            className="group flex items-center text-xs font-medium md:text-base lg:text-lg justify-center mt-6 text-red-700  hover:text-red-600"
-          >
+
+        <Link href="/about/y4s" legacyBehavior>
+          <a className="group flex items-center text-xs font-medium md:text-base lg:text-lg justify-center mt-6 text-red-700 hover:text-red-600">
             Learn more about Yes4Save Foundation
             <ArrowRight className="ml-2 size-4 transition-transform group-hover:translate-x-1" />
           </a>
-          </Link>
+        </Link>
+      </div>
     </section>
   );
 };
 
-export default Welcome ;
-
-  
+export default Welcome;
